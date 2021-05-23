03830aa56fde3a1b7948878c6841c18832d5e6b6
Roman <vovkroman2013@gmail.com>
2021-05-23 22:11:25 +0200

    asdasd

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,17 @@ function menuToggle(){
     toggleMenu.classList.toggle('active');
     section.classList.toggle('active');
 };
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', (e) => {
+        e.preventDefault ();
+    const blockId = anchor.getAttribute('href')
+    document.querySelector('' + blockId).scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+    })
+    })
+}
